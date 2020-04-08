d9c79062b1ca9df39ea5c5dcce18fc27435b9c9f
MrLIChuan <53934546+MrLIChuan@users.noreply.github.com>
2020-04-08 02:05:30 +0200

    BoutonBaladeur

---
+++ BoutonBaladeur/BoutonBaladeur/Animation.Designer.vb
@@ -0,0 +1,329 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmAnimation
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.pnlAnimation = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.cmdNotMe = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.Button13 = New System.Windows.Forms.Button()
+        Me.Button14 = New System.Windows.Forms.Button()
+        Me.Button15 = New System.Windows.Forms.Button()
+        Me.Button16 = New System.Windows.Forms.Button()
+        Me.Button17 = New System.Windows.Forms.Button()
+        Me.Button18 = New System.Windows.Forms.Button()
+        Me.Button19 = New System.Windows.Forms.Button()
+        Me.Button20 = New System.Windows.Forms.Button()
+        Me.Button21 = New System.Windows.Forms.Button()
+        Me.pnlAnimation.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'pnlAnimation
+        '
+        Me.pnlAnimation.Controls.Add(Me.Button17)
+        Me.pnlAnimation.Controls.Add(Me.Button18)
+        Me.pnlAnimation.Controls.Add(Me.Button19)
+        Me.pnlAnimation.Controls.Add(Me.Button20)
+        Me.pnlAnimation.Controls.Add(Me.Button21)
+        Me.pnlAnimation.Controls.Add(Me.Button12)
+        Me.pnlAnimation.Controls.Add(Me.Button13)
+        Me.pnlAnimation.Controls.Add(Me.Button14)
+        Me.pnlAnimation.Controls.Add(Me.Button15)
+        Me.pnlAnimation.Controls.Add(Me.Button16)
+        Me.pnlAnimation.Controls.Add(Me.Button7)
+        Me.pnlAnimation.Controls.Add(Me.Button8)
+        Me.pnlAnimation.Controls.Add(Me.Button9)
+        Me.pnlAnimation.Controls.Add(Me.Button10)
+        Me.pnlAnimation.Controls.Add(Me.Button11)
+        Me.pnlAnimation.Controls.Add(Me.Button6)
+        Me.pnlAnimation.Controls.Add(Me.Button5)
+        Me.pnlAnimation.Controls.Add(Me.Button4)
+        Me.pnlAnimation.Controls.Add(Me.Button3)
+        Me.pnlAnimation.Controls.Add(Me.Button2)
+        Me.pnlAnimation.Controls.Add(Me.Button1)
+        Me.pnlAnimation.Location = New System.Drawing.Point(12, 160)
+        Me.pnlAnimation.Name = "pnlAnimation"
+        Me.pnlAnimation.Size = New System.Drawing.Size(767, 395)
+        Me.pnlAnimation.TabIndex = 0
+        Me.pnlAnimation.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Tahoma", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(258, 71)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(244, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Catch ""me"" if you can!"
+        '
+        'cmdNotMe
+        '
+        Me.cmdNotMe.Location = New System.Drawing.Point(308, 117)
+        Me.cmdNotMe.Name = "cmdNotMe"
+        Me.cmdNotMe.Size = New System.Drawing.Size(112, 37)
+        Me.cmdNotMe.TabIndex = 1
+        Me.cmdNotMe.Text = "NOT ME"
+        Me.cmdNotMe.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(296, 31)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(112, 37)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = " ME"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(591, 91)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(112, 37)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = " ME"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(441, 91)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(112, 37)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = " ME"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(151, 91)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(112, 37)
+        Me.Button4.TabIndex = 5
+        Me.Button4.Text = " ME"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(296, 91)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(112, 37)
+        Me.Button5.TabIndex = 6
+        Me.Button5.Text = " ME"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(6, 91)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(112, 37)
+        Me.Button6.TabIndex = 7
+        Me.Button6.Text = " ME"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(6, 156)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(112, 37)
+        Me.Button7.TabIndex = 12
+        Me.Button7.Text = " ME"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(296, 156)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(112, 37)
+        Me.Button8.TabIndex = 11
+        Me.Button8.Text = " ME"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(151, 156)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(112, 37)
+        Me.Button9.TabIndex = 10
+        Me.Button9.Text = " ME"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(441, 156)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(112, 37)
+        Me.Button10.TabIndex = 9
+        Me.Button10.Text = " ME"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(591, 156)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(112, 37)
+        Me.Button11.TabIndex = 8
+        Me.Button11.Text = " ME"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Button12
+        '
+        Me.Button12.Location = New System.Drawing.Point(6, 213)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(112, 37)
+        Me.Button12.TabIndex = 17
+        Me.Button12.Text = " ME"
+        Me.Button12.UseVisualStyleBackColor = True
+        '
+        'Button13
+        '
+        Me.Button13.Location = New System.Drawing.Point(296, 213)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(112, 37)
+        Me.Button13.TabIndex = 16
+        Me.Button13.Text = " ME"
+        Me.Button13.UseVisualStyleBackColor = True
+        '
+        'Button14
+        '
+        Me.Button14.Location = New System.Drawing.Point(151, 213)
+        Me.Button14.Name = "Button14"
+        Me.Button14.Size = New System.Drawing.Size(112, 37)
+        Me.Button14.TabIndex = 15
+        Me.Button14.Text = " ME"
+        Me.Button14.UseVisualStyleBackColor = True
+        '
+        'Button15
+        '
+        Me.Button15.Location = New System.Drawing.Point(441, 213)
+        Me.Button15.Name = "Button15"
+        Me.Button15.Size = New System.Drawing.Size(112, 37)
+        Me.Button15.TabIndex = 14
+        Me.Button15.Text = " ME"
+        Me.Button15.UseVisualStyleBackColor = True
+        '
+        'Button16
+        '
+        Me.Button16.Location = New System.Drawing.Point(591, 213)
+        Me.Button16.Name = "Button16"
+        Me.Button16.Size = New System.Drawing.Size(112, 37)
+        Me.Button16.TabIndex = 13
+        Me.Button16.Text = " ME"
+        Me.Button16.UseVisualStyleBackColor = True
+        '
+        'Button17
+        '
+        Me.Button17.Location = New System.Drawing.Point(6, 281)
+        Me.Button17.Name = "Button17"
+        Me.Button17.Size = New System.Drawing.Size(112, 37)
+        Me.Button17.TabIndex = 22
+        Me.Button17.Text = " ME"
+        Me.Button17.UseVisualStyleBackColor = True
+        '
+        'Button18
+        '
+        Me.Button18.Location = New System.Drawing.Point(296, 281)
+        Me.Button18.Name = "Button18"
+        Me.Button18.Size = New System.Drawing.Size(112, 37)
+        Me.Button18.TabIndex = 21
+        Me.Button18.Text = " ME"
+        Me.Button18.UseVisualStyleBackColor = True
+        '
+        'Button19
+        '
+        Me.Button19.Location = New System.Drawing.Point(151, 281)
+        Me.Button19.Name = "Button19"
+        Me.Button19.Size = New System.Drawing.Size(112, 37)
+        Me.Button19.TabIndex = 20
+        Me.Button19.Text = " ME"
+        Me.Button19.UseVisualStyleBackColor = True
+        '
+        'Button20
+        '
+        Me.Button20.Location = New System.Drawing.Point(441, 281)
+        Me.Button20.Name = "Button20"
+        Me.Button20.Size = New System.Drawing.Size(112, 37)
+        Me.Button20.TabIndex = 19
+        Me.Button20.Text = " ME"
+        Me.Button20.UseVisualStyleBackColor = True
+        '
+        'Button21
+        '
+        Me.Button21.Location = New System.Drawing.Point(591, 281)
+        Me.Button21.Name = "Button21"
+        Me.Button21.Size = New System.Drawing.Size(112, 37)
+        Me.Button21.TabIndex = 18
+        Me.Button21.Text = " ME"
+        Me.Button21.UseVisualStyleBackColor = True
+        '
+        'frmAnimation
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(802, 594)
+        Me.Controls.Add(Me.cmdNotMe)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.pnlAnimation)
+        Me.Name = "frmAnimation"
+        Me.Text = "Animation"
+        Me.pnlAnimation.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents pnlAnimation As System.Windows.Forms.GroupBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents cmdNotMe As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button17 As System.Windows.Forms.Button
+    Friend WithEvents Button18 As System.Windows.Forms.Button
+    Friend WithEvents Button19 As System.Windows.Forms.Button
+    Friend WithEvents Button20 As System.Windows.Forms.Button
+    Friend WithEvents Button21 As System.Windows.Forms.Button
+    Friend WithEvents Button12 As System.Windows.Forms.Button
+    Friend WithEvents Button13 As System.Windows.Forms.Button
+    Friend WithEvents Button14 As System.Windows.Forms.Button
+    Friend WithEvents Button15 As System.Windows.Forms.Button
+    Friend WithEvents Button16 As System.Windows.Forms.Button
+    Friend WithEvents Button7 As System.Windows.Forms.Button
+    Friend WithEvents Button8 As System.Windows.Forms.Button
+    Friend WithEvents Button9 As System.Windows.Forms.Button
+    Friend WithEvents Button10 As System.Windows.Forms.Button
+    Friend WithEvents Button11 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+
+End Class
---
+++ BoutonBaladeur/BoutonBaladeur/Animation.vb
@@ -0,0 +1,39 @@
+﻿Public Class frmAnimation
+    Dim cmdMe(21) As Button
+
+    Private Sub frmAnimation_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+
+        For i = 1 To 21
+            cmdMe(i) = pnlAnimation.Controls("Button" & i)
+            cmdMe(i).Visible = False
+        Next
+
+        cmdMe(1).Visible = True
+
+        For Each x As Button In pnlAnimation.Controls
+            AddHandler x.MouseMove, AddressOf BoutonBaladeur
+
+
+        Next
+
+    End Sub
+
+    Private Sub BoutonBaladeur(ByVal sender As Object, ByVal e As System.EventArgs)
+
+
+
+        Dim k As Byte
+
+        Randomize()
+        sender.visible = False
+        k = Int(Rnd() * 21) + 1
+        cmdMe(k).Visible = True
+
+    End Sub
+
+
+    Private Sub pnlAnimation_Enter(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles pnlAnimation.Enter
+
+    End Sub
+End Class
